058686763160af9431dfc928a9539863f314b46d
alamhamim <82621331+hamim-exe@users.noreply.github.com>
2022-02-19 17:56:00 -0500

    commit from main

--- cypress/integration/Rest-API-Test/practice.spec.ts
+++ cypress/integration/Rest-API-Test/practice.spec.ts
@@ -6,4 +6,7 @@ describe('Hello world', () => {
     it('Test two', () => {
         cy.log('This is test two')
     });
+    it('Test three', () => {
+        cy.log('This is test three')
+    });
 });
